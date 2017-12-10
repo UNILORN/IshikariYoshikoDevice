a3816a0ab9b1e2a0a65fa072a858847a0f088eb6
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-12-11 01:04:27 +0900

    add

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("aa")
+#-*- coding:utf-8 -*-
+#
+# Copyright (c) OPTiM Corporation 2017. All rights reserved.
+# Permission to use, copy, modify and redistribute are strongly controlled
+# under the rights of OPTiM Corporation.
+#
+
+import CiosRaspberryHat
+#import SensorPutLed
+import sys
+import os
+import time
+from os.path import join, dirname
+
+if __name__ == '__main__':
+    url = "wss://api.optim.cloud/v1/messaging"
+    channel = ""
+
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    token = os.environ.get("ACCESS_TOKEN")
+
+    cios = CiosRaspberryHat.CiosRaspberryHat(url,channel,token)
+    #gyro = GyroscopeLed.GyroscopeLed()
+
+    message = cios.getSensorData()
+    #sp = SensorPutLed.SensorPutLed(message)
+    
+    cios.connection()
+ 
+    LedTime = 0.01
+
+    while True:
+        time.sleep(LedTime)
+        message = cios.getSensorData()
+        
+        if message != "error":
+            sp.setSensorData(message)
+
+        cios.sendMessage(message)
